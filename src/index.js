dbfba331eb2a91b0d641d5bef7285517d005428a
Chamod De SIlva <chamodlakshitha12@gmail.com>
2024-12-26 13:54:56 +0530

    project initialized

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<React.StrictMode></React.StrictMode>);
